637aa23eb4d253e769993328ff4462a616bf60c9
shawonstack <shawon934764@gmail.com>
2026-06-27 17:07:03 +0600

    counter done

---
+++ counter.js
@@ -0,0 +1,27 @@
+function creatCounter() {
+  let number = 0;
+  return function () {
+    number++;
+    return number;
+  };
+}
+const result = creatCounter();
+// -------
+function creatCslculatior() {
+  let count = 10;
+  return function () {
+    count++;
+    return count;
+  };
+}
+const calculator = creatCslculatior();
+console.log(calculator());
+function passwordLonger() {
+  let password = 0;
+  return function () {
+    password++;
+    return password;
+  };
+}
+const pass = passwordLonger();
+console.log(pass());
