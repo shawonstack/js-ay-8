4e057945227ab68f7307f41b8c66345eed3a6d90
shawonstack <shawon934764@gmail.com>
2026-06-28 18:49:34 +0600

    task 1 and tas2 done

--- task.js
+++ task.js
@@ -1,9 +1,36 @@
-const obj = {};
-
-let key = 'age';
-
-obj[key] = 25;
-
-console.log(obj.age);
-console.log(obj['age']);
-console.log(obj[key]);
+function makeCounter() {
+  let count = 10;
+  return {
+    increment() {
+      count++;
+      return count;
+    },
+    decrement() {
+      count--;
+      return count;
+    },
+    reset() {
+      count = 10;
+      return count;
+    },
+  };
+}
+const counter = makeCounter();
+console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.reset());
+// -----------------task2
+function creatUser(name) {
+  let password = '12345';
+  return {
+    getName() {
+      return name;
+    },
+    getPass(pass) {
+      return pass === password;
+    },
+  };
+}
+const user = creatUser('shawon');
+console.log(user.getName());
+console.log(user.getPass('3456'));
